449d65210c6b48d96d35eb6e94fd6567a02459fe
shivamtg <shivamtg1376@gmail.com>
2022-07-29 11:39:11 +0200

    0.S ex

---
+++ SOLID/0.S/new.js
@@ -0,0 +1,159 @@
+var Car = /** @class */ (function () {
+    function Car(MAXIMUM_FUEL_CAPACITY) {
+        this._fuel = 0;
+        this._miles = 0;
+        this.FUEL_MILEAGE = 10;
+        this._radio = new Radio();
+        this._engine = new Engine();
+        this.MAXIMUM_FUEL_CAPACITY = MAXIMUM_FUEL_CAPACITY;
+    }
+    Object.defineProperty(Car.prototype, "miles", {
+        get: function () {
+            return this._miles;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Car.prototype, "fuel", {
+        //Take attention to these getter and setters
+        get: function () {
+            return this._fuel;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Car.prototype, "radio", {
+        get: function () {
+            return this._radio;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Car.prototype, "engine", {
+        get: function () {
+            return this._engine;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    //When a value can only go one way (you add fuel, consuming fuel is handled by the car itself)
+    // it is better to provide a specific method for this instead of a generic setter.
+    // with a setter there is always the chance of somebody lowering the fuel amount by accident.
+    Car.prototype.addFuel = function (fuel) {
+        this._fuel = Math.min(fuel + this._fuel, this.MAXIMUM_FUEL_CAPACITY);
+    };
+    Car.prototype.drive = function () {
+        if (this._engine.engineStatus === false || this._fuel <= 0) {
+            //what I am doing here is a good principle called "failing early"
+            // If you have some conditions you need to check, that will exclude most of the code in your function check that first
+            // This prevents your "happy path" of code to be deeply indented.
+            return;
+        }
+        this._fuel -= 1;
+        this._miles += this.FUEL_MILEAGE;
+    };
+    return Car;
+}());
+//Shiva - Here is the radio class... since this project is about single responsibility I figured that radio can be a separate object that has the responsibilities regarding the radio
+var Radio = /** @class */ (function () {
+    function Radio() {
+        this._musicLevel = 0;
+        this._oldMusicLevel = 50;
+    }
+    Object.defineProperty(Radio.prototype, "musicLevel", {
+        get: function () {
+            return this._musicLevel;
+        },
+        set: function (value) {
+            this._musicLevel = value;
+            this._oldMusicLevel = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Radio.prototype.turnMusicOn = function () {
+        this._musicLevel = this._oldMusicLevel;
+    };
+    Radio.prototype.turnMusicOff = function () {
+        this._musicLevel = 0;
+    };
+    return Radio;
+}());
+//Shiva - I also made an engine class... if radio can be an object, can't the engine also be one?
+var Engine = /** @class */ (function () {
+    function Engine() {
+        this._engineStatus = false;
+    }
+    Object.defineProperty(Engine.prototype, "engineStatus", {
+        get: function () {
+            return this._engineStatus;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Engine.prototype.turnEngineOn = function () {
+        this._engineStatus = true;
+    };
+    Engine.prototype.turnEngineOff = function () {
+        this._engineStatus = false;
+    };
+    return Engine;
+}());
+//Shiva - There were some things I needed to change here... whenever some method related to the radio or engin is called now needs to be called through car and then its class.
+var musicToggleElement = document.querySelector('#music-toggle');
+var musicSliderElement = document.querySelector('#music-slider');
+var engineToggleElement = document.querySelector('#engine-toggle');
+var addFuelForm = document.querySelector('#add-fuel-form');
+var addFuelInput = document.querySelector('#add-fuel-input');
+var fuelLevelElement = document.querySelector('#fuel-level');
+var milesElement = document.querySelector('#miles-value');
+var audioElement = document.querySelector('#car-music');
+var car = new Car(100);
+musicToggleElement.addEventListener('click', function () {
+    if (car.radio.musicLevel === 0) {
+        car.radio.turnMusicOn();
+        musicSliderElement.value = car.radio.musicLevel.toString();
+        musicToggleElement.innerText = 'Turn music off';
+        return;
+    }
+    musicToggleElement.innerText = 'Turn music on';
+    car.radio.turnMusicOff();
+});
+//I use input instead of change, because then the value changes when I move the mouse, not only on release
+musicSliderElement.addEventListener('input', function (event) {
+    var target = (event.target);
+    var musicLevel = car.radio.musicLevel;
+    musicLevel = target.value;
+    audioElement.volume = musicLevel / 100;
+    //@todo when you are repeating the same text over and over again maybe we should have made some constants for it? Can you do improve on this?
+    //Shiva - I made a variable for music level
+    musicToggleElement.innerText = musicLevel ? 'Turn music off' : 'Turn music on';
+});
+engineToggleElement.addEventListener('click', function () {
+    if (car.engine.engineStatus) {
+        car.engine.turnEngineOff();
+        engineToggleElement.innerText = 'Turn engine on';
+        return;
+    }
+    engineToggleElement.innerText = 'Turn engine off';
+    car.engine.turnEngineOn();
+});
+addFuelForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    car.addFuel(Number(addFuelInput.value));
+    fuelLevelElement.innerText = car.fuel.toString();
+});
+setInterval(function () {
+    car.drive();
+    //while it looks like both lines below are the same there is a subtle difference (you could put breakpoints here to see the difference):
+    // this <cast> will only tell TypeScript that the value is a string, but the actual variable in JS is not changed in any way: it is in reality still a number
+    milesElement.innerText = (car.miles);
+    // This .toString() will actually convert the value in JavaScript from an integer to a string
+    fuelLevelElement.innerText = car.fuel.toString();
+    if (car.radio.musicLevel === 0) {
+        audioElement.pause();
+    }
+    else {
+        audioElement.play();
+    }
+}, 1000);
