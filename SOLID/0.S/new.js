0dc2b6ded387d586d0bd3dcddc57b544696f1656
shivamtg <shivamtg1376@gmail.com>
2022-08-01 10:21:47 +0200

    add fuel to the engine class in ex 0.S

--- SOLID/0.S/new.js
+++ SOLID/0.S/new.js
@@ -1,20 +1,16 @@
 "use strict";
 class Car {
+    //By changing this variable to readonly I have in essence created a property constant.
+    // the only subtle difference is that you can write once to the variable inside the constructor
     constructor(MAXIMUM_FUEL_CAPACITY) {
-        this._fuel = 0;
         this._miles = 0;
-        this.FUEL_MILEAGE = 10;
         this._radio = new Radio();
-        this._engine = new Engine();
-        this.MAXIMUM_FUEL_CAPACITY = MAXIMUM_FUEL_CAPACITY;
+        this._engine = new Engine(MAXIMUM_FUEL_CAPACITY);
     }
     get miles() {
         return this._miles;
     }
     //Take attention to these getter and setters
-    get fuel() {
-        return this._fuel;
-    }
     get radio() {
         return this._radio;
     }
@@ -24,18 +20,15 @@ class Car {
     //When a value can only go one way (you add fuel, consuming fuel is handled by the car itself)
     // it is better to provide a specific method for this instead of a generic setter.
     // with a setter there is always the chance of somebody lowering the fuel amount by accident.
-    addFuel(fuel) {
-        this._fuel = Math.min(fuel + this._fuel, this.MAXIMUM_FUEL_CAPACITY);
-    }
     drive() {
-        if (this._engine.engineStatus === false || this._fuel <= 0) {
+        if (this._engine.engineStatus === false || this.engine.fuel <= 0) {
             //what I am doing here is a good principle called "failing early"
             // If you have some conditions you need to check, that will exclude most of the code in your function check that first
             // This prevents your "happy path" of code to be deeply indented.
             return;
         }
-        this._fuel -= 1;
-        this._miles += this.FUEL_MILEAGE;
+        this.engine.handleFuel();
+        this._miles += this.engine.FUEL_MILEAGE;
     }
 }
 //Shiva - Here is the radio class... since this project is about single responsibility I figured that radio can be a separate object that has the responsibilities regarding the radio
@@ -60,8 +53,11 @@ class Radio {
 }
 //Shiva - I also made an engine class... if radio can be an object, can't the engine also be one?
 class Engine {
-    constructor() {
+    constructor(MAXIMUM_FUEL_CAPACITY) {
         this._engineStatus = false;
+        this._fuel = 0;
+        this._FUEL_MILEAGE = 10;
+        this.MAXIMUM_FUEL_CAPACITY = MAXIMUM_FUEL_CAPACITY;
     }
     get engineStatus() {
         return this._engineStatus;
@@ -71,6 +67,18 @@ class Engine {
     }
     turnEngineOff() {
         this._engineStatus = false;
+    }
+    addFuel(fuel) {
+        this._fuel = Math.min(fuel + this._fuel, this.MAXIMUM_FUEL_CAPACITY);
+    }
+    get fuel() {
+        return this._fuel;
+    }
+    get FUEL_MILEAGE() {
+        return this._FUEL_MILEAGE;
+    }
+    handleFuel() {
+        this._fuel -= 1;
     }
 }
 //Shiva - There were some things I needed to change here... whenever some method related to the radio or engin is called now needs to be called through car and then its class.
@@ -96,12 +104,10 @@ musicToggleElement.addEventListener('click', () => {
 //I use input instead of change, because then the value changes when I move the mouse, not only on release
 musicSliderElement.addEventListener('input', (event) => {
     let target = (event.target);
-    let musicLevel = car.radio.musicLevel;
-    musicLevel = target.value;
-    audioElement.volume = musicLevel / 100;
+    car.radio.musicLevel = target.value;
+    audioElement.volume = car.radio.musicLevel / 100;
     //@todo when you are repeating the same text over and over again maybe we should have made some constants for it? Can you do improve on this?
-    //Shiva - I made a variable for music level
-    musicToggleElement.innerText = musicLevel ? 'Turn music off' : 'Turn music on';
+    musicToggleElement.innerText = car.radio.musicLevel ? 'Turn music off' : 'Turn music on';
 });
 engineToggleElement.addEventListener('click', () => {
     if (car.engine.engineStatus) {
@@ -114,8 +120,8 @@ engineToggleElement.addEventListener('click', () => {
 });
 addFuelForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    car.addFuel(Number(addFuelInput.value));
-    fuelLevelElement.innerText = car.fuel.toString();
+    car.engine.addFuel(Number(addFuelInput.value));
+    fuelLevelElement.innerText = car.engine.fuel.toString();
 });
 setInterval(() => {
     car.drive();
@@ -123,7 +129,7 @@ setInterval(() => {
     // this <cast> will only tell TypeScript that the value is a string, but the actual variable in JS is not changed in any way: it is in reality still a number
     milesElement.innerText = (car.miles);
     // This .toString() will actually convert the value in JavaScript from an integer to a string
-    fuelLevelElement.innerText = car.fuel.toString();
+    fuelLevelElement.innerText = car.engine.fuel.toString();
     if (car.radio.musicLevel === 0) {
         audioElement.pause();
     }
--- SOLID/0.S/new.ts
+++ SOLID/0.S/new.ts
@@ -4,18 +4,14 @@ class Car {
     //Shiva - I removed all the radio related and engine related properties and methods and made separate classes for them .
     private _radio: Radio;
     private _engine: Engine;
-    private _fuel : number = 0;
     private _miles : number = 0;
 
     //By changing this variable to readonly I have in essence created a property constant.
     // the only subtle difference is that you can write once to the variable inside the constructor
-    private readonly MAXIMUM_FUEL_CAPACITY: number;
-    private readonly FUEL_MILEAGE: number = 10;
 
     constructor(MAXIMUM_FUEL_CAPACITY: number) {
         this._radio = new Radio();
-        this._engine = new Engine();
-        this.MAXIMUM_FUEL_CAPACITY = MAXIMUM_FUEL_CAPACITY;
+        this._engine = new Engine(MAXIMUM_FUEL_CAPACITY);
     }
 
     get miles(): number {
@@ -23,9 +19,6 @@ class Car {
     }
 
 //Take attention to these getter and setters
-    get fuel(): number {
-        return this._fuel;
-    }
 
     get radio(): Radio {
         return this._radio;
@@ -39,20 +32,18 @@ class Car {
 //When a value can only go one way (you add fuel, consuming fuel is handled by the car itself)
     // it is better to provide a specific method for this instead of a generic setter.
     // with a setter there is always the chance of somebody lowering the fuel amount by accident.
-    addFuel(fuel : number) {
-        this._fuel = Math.min(fuel + this._fuel, this.MAXIMUM_FUEL_CAPACITY);
-    }
+
 
     drive() {
-        if(this._engine.engineStatus === false || this._fuel <= 0) {
+        if(this._engine.engineStatus === false || this.engine.fuel <= 0) {
             //what I am doing here is a good principle called "failing early"
             // If you have some conditions you need to check, that will exclude most of the code in your function check that first
             // This prevents your "happy path" of code to be deeply indented.
             return;
         }
 
-        this._fuel -= 1;
-        this._miles += this.FUEL_MILEAGE;
+        this.engine.handleFuel();
+        this._miles += this.engine.FUEL_MILEAGE;
     }
 }
 //Shiva - Here is the radio class... since this project is about single responsibility I figured that radio can be a separate object that has the responsibilities regarding the radio
@@ -80,7 +71,14 @@ class Radio{
 //Shiva - I also made an engine class... if radio can be an object, can't the engine also be one?
 class Engine{
     private _engineStatus: boolean = false;
+    private _fuel : number = 0;
 
+    private readonly MAXIMUM_FUEL_CAPACITY: number;
+    private readonly _FUEL_MILEAGE: number = 10;
+
+    constructor(MAXIMUM_FUEL_CAPACITY: number) {
+        this.MAXIMUM_FUEL_CAPACITY = MAXIMUM_FUEL_CAPACITY;
+    }
     get engineStatus(): boolean {
         return this._engineStatus;
     }
@@ -91,6 +89,22 @@ class Engine{
 
     turnEngineOff() {
         this._engineStatus = false;
+    }
+
+    addFuel(fuel : number) {
+        this._fuel = Math.min(fuel + this._fuel, this.MAXIMUM_FUEL_CAPACITY);
+    }
+
+    get fuel(): number {
+        return this._fuel;
+    }
+
+    get FUEL_MILEAGE(): number {
+        return this._FUEL_MILEAGE;
+    }
+
+    handleFuel() {
+        this._fuel -= 1;
     }
 }
 //Shiva - There were some things I needed to change here... whenever some method related to the radio or engin is called now needs to be called through car and then its class.
@@ -119,14 +133,12 @@ musicToggleElement.addEventListener('click', () => {
 //I use input instead of change, because then the value changes when I move the mouse, not only on release
 musicSliderElement.addEventListener('input', (event) => {
     let target = <HTMLFormElement>(event.target);
-    let musicLevel = car.radio.musicLevel;
 
-    musicLevel = target.value;
-    audioElement.volume = musicLevel / 100;
+    car.radio.musicLevel = target.value;
+    audioElement.volume = car.radio.musicLevel / 100;
 
     //@todo when you are repeating the same text over and over again maybe we should have made some constants for it? Can you do improve on this?
-    //Shiva - I made a variable for music level
-    musicToggleElement.innerText = musicLevel ? 'Turn music off' : 'Turn music on';
+    musicToggleElement.innerText = car.radio.musicLevel ? 'Turn music off' : 'Turn music on';
 });
 
 engineToggleElement.addEventListener('click', () => {
@@ -142,8 +154,8 @@ engineToggleElement.addEventListener('click', () => {
 addFuelForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    car.addFuel(Number(addFuelInput.value));
-    fuelLevelElement.innerText = car.fuel.toString();
+    car.engine.addFuel(Number(addFuelInput.value));
+    fuelLevelElement.innerText = car.engine.fuel.toString();
 });
 
 setInterval(() => {
@@ -153,7 +165,7 @@ setInterval(() => {
     // this <cast> will only tell TypeScript that the value is a string, but the actual variable in JS is not changed in any way: it is in reality still a number
     milesElement.innerText = <string><unknown>(car.miles);
     // This .toString() will actually convert the value in JavaScript from an integer to a string
-    fuelLevelElement.innerText = car.fuel.toString();
+    fuelLevelElement.innerText = car.engine.fuel.toString();
 
     if(car.radio.musicLevel === 0) {
         audioElement.pause();
