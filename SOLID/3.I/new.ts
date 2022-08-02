77b0f06236ff9e64781a56f5481d150637dcd6fd
shivamtg <shivamtg1376@gmail.com>
2022-08-02 15:57:48 +0200

    3.I add google bot

--- SOLID/3.I/new.ts
+++ SOLID/3.I/new.ts
@@ -1,38 +1,46 @@
-interface UserAuth {
-    setGoogleToken(token : string):void;
-    checkGoogleLogin(token : string) : boolean;
-    setFacebookToken(token : string):void;
-    getFacebookLogin(token : string) : boolean;
+interface UserAuthWithGoogle {
+    setGoogleToken(token: string): void;
+
+    checkGoogleLogin(token: string): boolean;
 }
-interface UserAuthWithPass{
-    checkPassword(password: string) : boolean;
-    resetPassword() : void;
+
+interface UserAuthWithFacebook {
+    setFacebookToken(token: string): void;
+
+    getFacebookLogin(token: string): boolean;
 }
-class User implements UserAuth, UserAuthWithPass {
-    private _password : string = 'user';
-    private _facebookToken : string;
-    private _googleToken : string;
+
+interface UserAuthWithPass {
+    checkPassword(password: string): boolean;
+
+    resetPassword(): void;
+}
+
+class User implements UserAuthWithGoogle, UserAuthWithPass, UserAuthWithFacebook {
+    private _password: string = 'user';
+    private _facebookToken: string;
+    private _googleToken: string;
 
     //Interesting detail here: while I did not define a return type or param type, any deviation from the interface will give you an error.
     // Test it out by uncommenting the code below.
-    checkGoogleLogin(token:string) {
+    checkGoogleLogin(token: string) {
         // return "this will not work";
         return (token === this._googleToken);
     }
 
-    setGoogleToken(token : string) {
+    setGoogleToken(token: string) {
         this._googleToken = token;
     }
 
-    getFacebookLogin(token:string) {
+    getFacebookLogin(token: string) {
         return (token === this._facebookToken);
     }
 
-    setFacebookToken(token : string) {
+    setFacebookToken(token: string) {
         this._facebookToken = token;
     }
 
-    checkPassword(password: string) : boolean {
+    checkPassword(password: string): boolean {
         return (password === this._password);
     }
 
@@ -43,7 +51,7 @@ class User implements UserAuth, UserAuthWithPass {
 
 //admin cannot use google or facebook token
 class Admin implements UserAuthWithPass {
-    private _password : string = 'admin';
+    private _password: string = 'admin';
 
 
     checkPassword(password: string): boolean {
@@ -55,7 +63,17 @@ class Admin implements UserAuthWithPass {
     }
 }
 
-// class GoogleBot implements UserAuth {}
+class GoogleBot implements UserAuthWithGoogle {
+    private _googleToken: string;
+
+    checkGoogleLogin(token: string) {
+        return (token === this._googleToken);
+    }
+
+    setGoogleToken(token: string) {
+        this._googleToken = token;
+    }
+}
 
 const passwordElement = <HTMLInputElement>document.querySelector('#password');
 const typePasswordElement = <HTMLInputElement>document.querySelector('#typePassword');
@@ -66,36 +84,45 @@ const resetPasswordElement = <HTMLAnchorElement>document.querySelector('#resetPa
 
 let guest = new User;
 let admin = new Admin;
-
+let google = new GoogleBot;
 document.querySelector('#login-form').addEventListener('submit', (event) => {
     event.preventDefault();
-
-    let user = loginAsAdminElement.checked ? admin : guest;
+    let user;
+    if (loginAsAdminElement.checked) {
+        user = admin;
+    } else if (!loginAsAdminElement.checked && typeGoogleElement.checked) {
+        user = google;
+    } else {
+        user = guest;
+    }
     let auth = false;
-    if(user == guest) {
-        user.setGoogleToken('secret_token_google');
+    if (user == guest) {
+        //user.setGoogleToken('secret_token_google');
         user.setFacebookToken('secret_token_fb');
-        switch(true) {
+        switch (true) {
             case typePasswordElement.checked:
                 auth = user.checkPassword(passwordElement.value);
                 break;
-            case typeGoogleElement.checked:
+            /*case typeGoogleElement.checked:
                 auth = user.checkGoogleLogin('secret_token_google');
-                break;
+                break;*/
             case typeFacebookElement.checked:
                 debugger;
                 auth = user.getFacebookLogin('secret_token_fb');
                 break;
         }
-    }else {
-        if (!typeGoogleElement.checked && !typeFacebookElement.checked){
+    } else if (user == google) {
+        user.setGoogleToken('secret_token_google');
+        auth = user.checkGoogleLogin('secret_token_google');
+    } else if(user == admin){
+        if (!typeGoogleElement.checked && !typeFacebookElement.checked) {
             auth = user.checkPassword(passwordElement.value);
         }
     }
     debugger;
 
 
-    if(auth) {
+    if (auth) {
         alert('login success');
     } else {
         alert('login failed');
