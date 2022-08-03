2dcb09a530cee39d9ab76a3fdf657d3b158e2920
shivamtg <shivamtg1376@gmail.com>
2022-08-03 11:13:18 +0200

    4.D comment the old version

--- SOLID/4.D/old.ts
+++ SOLID/4.D/old.ts
@@ -1,3 +1,4 @@
+/*
 class Oven {
     private _isOn : boolean;
 
@@ -58,10 +59,11 @@ let bakery = new Restaurant("Bakery");
 bakery.Cook("cookies");
 
 //Now if we want to add a new restaurant with an ELECTRIC cooker, we are gonna be in a hot mess ...
-/*
+/!*
 let bakery = new Restaurant("Bakery", new Oven());
 bakery.Cook("cookies");
 
 let crepery = new Restaurant("Crepery", new Stove());
 crepery.Cook("crepes");
- */
+ *!/
+*/
